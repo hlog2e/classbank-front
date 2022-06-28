9d135dde3812b7400eda9597997ed2daf3f83403
HongRok Kim <hlog2e@gmail.com>
2022-06-28 14:50:42 +0900

    HomePanel 내부의 비즈니스 로직 State 분리

--- components/teacher/home/HomePanel.jsx
+++ components/teacher/home/HomePanel.jsx
@@ -1,14 +1,11 @@
 import { useState } from "react";
 import { BsCoin } from "react-icons/bs";
 
-export default function HomePanel() {
-  const [moneyName, setMoneyName] = useState("도지코인");
+export default function HomePanel(props) {
   const [onChangeMoneyName, setOnChangeMoneyName] = useState(false);
 
-  const [eza, setEza] = useState(10);
   const [onChangeEza, setOnChangeEza] = useState(false);
 
-  const [ezaTerm, setEzaTerm] = useState("7");
   const [onChangeEzaTerm, setOnChangeEzaTerm] = useState(false);
   return (
     <>
@@ -19,7 +16,9 @@ export default function HomePanel() {
             <>
               <div className="flex items-center ">
                 <BsCoin className="text-gray-500" size="30px" />
-                <p className="ml-2 text-xl font-bold">{moneyName}</p>
+                <p className="ml-2 text-xl font-bold">
+                  {props.panelData.moneyName}
+                </p>
               </div>
               <button
                 className="py-2 font-semibold text-white bg-blue-500 rounded-xl"
@@ -34,9 +33,12 @@ export default function HomePanel() {
             <>
               <input
                 className="px-2 py-1 text-xl font-bold bg-transparent border-b-2 focus:outline-none"
-                value={moneyName}
+                value={props.panelData.moneyName}
                 onChange={(e) => {
-                  setMoneyName(e.target.value);
+                  props.setPanelData({
+                    ...props.panelData,
+                    moneyName: e.target.value,
+                  });
                 }}
               />
               <button
@@ -52,17 +54,21 @@ export default function HomePanel() {
         </div>
         <div className="h-48 p-6 m-2 min-w-[170px] bg-white flex-1 drop-shadow-xl rounded-3xl">
           <h1 className="text-xl font-bold">판매중인 상품</h1>
-          <p className="mt-6 text-5xl font-semibold">10개</p>
+          <p className="mt-6 text-5xl font-semibold">
+            {props.panelData.sellingItemCount}개
+          </p>
         </div>
         <div className="h-48 p-6 m-2 min-w-[170px]  bg-white  flex-1 drop-shadow-xl rounded-3xl">
           <h1 className="text-xl font-bold">구입승인 대기 중</h1>
-          <p className="mt-6 text-5xl font-semibold">10개</p>
+          <p className="mt-6 text-5xl font-semibold">
+            {props.panelData.pendingBuyItemCount}개
+          </p>
         </div>
         <div className="h-48 p-6 m-2 min-w-[170px]  bg-white  flex flex-col justify-between   flex-1 drop-shadow-xl rounded-3xl">
           <h1 className="text-2xl font-bold">이율</h1>
           {!onChangeEza ? (
             <>
-              <p className="text-4xl font-semibold ">{eza}%</p>
+              <p className="text-4xl font-semibold ">{props.panelData.eza}%</p>
               <button
                 className="py-2 font-semibold text-white bg-blue-500 rounded-xl"
                 onClick={() => {
@@ -76,9 +82,12 @@ export default function HomePanel() {
             <>
               <input
                 className="px-2 py-1 text-xl font-bold bg-transparent border-b-2 focus:outline-none "
-                value={eza}
+                value={props.panelData.eza}
                 onChange={(e) => {
-                  setEza(e.target.value);
+                  props.setPanelData({
+                    ...props.panelData,
+                    eza: e.target.value,
+                  });
                 }}
               />
               <button
@@ -96,7 +105,9 @@ export default function HomePanel() {
           <h1 className="text-xl font-bold">이자 지급 주기</h1>
           {!onChangeEzaTerm ? (
             <>
-              <p className="text-4xl font-semibold ">{ezaTerm}일</p>
+              <p className="text-4xl font-semibold ">
+                {props.panelData.ezaTerm}일
+              </p>
               <button
                 className="py-2 font-semibold text-white bg-blue-500 rounded-xl"
                 onClick={() => {
@@ -110,9 +121,12 @@ export default function HomePanel() {
             <>
               <input
                 className="px-2 py-1 text-xl font-bold bg-transparent border-b-2 focus:outline-none "
-                value={ezaTerm}
+                value={props.panelData.ezaTerm}
                 onChange={(e) => {
-                  setEzaTerm(e.target.value);
+                  props.setPanelData({
+                    ...props.panelData,
+                    ezaTerm: e.target.value,
+                  });
                 }}
               />
               <button
--- pages/teacher/home.jsx
+++ pages/teacher/home.jsx
@@ -1,14 +1,27 @@
 import TSideBar from "../../components/teacher/TSideBar";
 import HomePanel from "../../components/teacher/home/HomePanel";
 import HomeHeader from "../../components/teacher/home/HomeHeader";
+import { useEffect, useState } from "react";
 
 export default function TeacherHome() {
+  //API 연결전까지 더미데이터
+  const [panelData, setPanelData] = useState({
+    moneyName: "메소",
+    sellingItemCount: "15",
+    pendingBuyItemCount: "2",
+    eza: "10",
+    ezaTerm: "7",
+  });
+
+  useEffect(() => {
+    console.log(panelData);
+  }, [panelData]);
   return (
     <div className="flex flex-col h-screen lg:flex-row bg-neutral-100">
       <TSideBar />
       <section className="flex flex-col w-full lg:w-[calc(100vw-288px)] h-full ">
         <HomeHeader />
-        <HomePanel />
+        <HomePanel panelData={panelData} setPanelData={setPanelData} />
       </section>
     </div>
   );
