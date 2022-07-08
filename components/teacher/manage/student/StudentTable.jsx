28395e50c472659a73660d2feb88c22469e962b8
HongRok Kim <hlog2e@gmail.com>
2022-07-08 11:28:58 +0900

    Teacher Manage Student Account Table 완성

---
+++ components/teacher/manage/student/StudentTable.jsx
@@ -0,0 +1,149 @@
+import { useState } from "react";
+
+export default function StudentTable(props) {
+  return (
+    <section className="px-6 py-14 xl:px-20 xl:py-8">
+      <p className="ml-3 text-2xl font-semibold text-slate-500">
+        선택된 학생 {props.selectedStudent.length}명
+      </p>
+      <header className="h-12 m-2 ">
+        <button className="px-3 py-2 m-1 font-semibold text-white bg-blue-500 rounded-xl">
+          비밀번호 초기화
+        </button>
+        <button className="px-3 py-2 m-1 font-semibold text-white bg-blue-500 rounded-xl">
+          회원탈퇴
+        </button>
+      </header>
+      <div className="bg-white w-[900px] py-6 px-6 rounded-3xl drop-shadow-lg">
+        <table className="w-full border-separate border-spacing-0">
+          <thead className=" bg-slate-100">
+            <tr className="h-12 border-b ">
+              <th className="w-10 rounded-l-xl">
+                <input
+                  className="text-blue-400 rounded bg-slate-200 border-slate-400 focus:ring-blue-500"
+                  type="checkbox"
+                  onChange={(e) => {
+                    props.handleSelectAll(e.target.checked);
+                  }}
+                  checked={
+                    props.selectedStudent.length === props.studentsInfo.length
+                  }
+                ></input>
+              </th>
+              <th className="w-16">학번</th>
+              <th className="w-24">이름</th>
+              <th className="w-36">아이디</th>
+              <th className="w-36">전화번호</th>
+              <th className="w-32 ">가입일자</th>
+              <th className="w-16 rounded-r-xl">수정</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.studentsInfo.map((item) => {
+              return (
+                <StudentTableItem
+                  studentData={item}
+                  selectedStudent={props.selectedStudent}
+                  handleSelectedStudents={props.handleSelectedStudents}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+}
+
+function StudentTableItem(props) {
+  const [editing, setEditing] = useState(false);
+  const [studentData, setStudentData] = useState(props.studentData);
+
+  function handleUpdateStudentInfo() {
+    if (editing) {
+      setEditing(false);
+      alert("db로 전송로직");
+    } else {
+      setEditing(true);
+    }
+  }
+  return (
+    <tr className="font-semibold text-center h-14 text-slate-400 ">
+      <td className="rounded-l-xl">
+        <input
+          className="text-blue-400 rounded bg-slate-200 border-slate-400 focus:ring-blue-500"
+          type="checkbox"
+          onChange={() => {
+            props.handleSelectedStudents(studentData.id);
+          }}
+          checked={props.selectedStudent.some(
+            (item) => item.id === studentData.id
+          )}
+        ></input>
+      </td>
+      <td className="px-1">
+        {editing ? (
+          <input
+            className="w-full h-8 text-center rounded-lg bg-neutral-100"
+            value={studentData.number}
+            onChange={(e) => {
+              setStudentData({ ...studentData, number: e.target.value });
+            }}
+          />
+        ) : (
+          <p>{studentData.number}</p>
+        )}
+      </td>
+      <td className="px-1 ">
+        {editing ? (
+          <input
+            className="w-full h-8 text-center rounded-lg bg-neutral-100"
+            value={studentData.name}
+            onChange={(e) => {
+              setStudentData({ ...studentData, name: e.target.value });
+            }}
+          />
+        ) : (
+          <p>{studentData.name}</p>
+        )}
+      </td>
+      <td className="px-1 ">
+        {editing ? (
+          <input
+            className="w-full h-8 text-center rounded-lg bg-neutral-100"
+            value={studentData.userId}
+            onChange={(e) => {
+              setStudentData({ ...studentData, userId: e.target.value });
+            }}
+          />
+        ) : (
+          <p>{studentData.userId}</p>
+        )}
+      </td>
+      <td className="px-1 ">
+        {editing ? (
+          <input
+            className="w-full h-8 text-center rounded-lg bg-neutral-100"
+            value={studentData.phoneNumber}
+            onChange={(e) => {
+              setStudentData({ ...studentData, phoneNumber: e.target.value });
+            }}
+          />
+        ) : (
+          <p>{studentData.phoneNumber}</p>
+        )}
+      </td>
+      <td className="px-1 rounded-r-xl">
+        <p>2022-02-07</p>
+      </td>
+      <td className="px-1 rounded-r-xl">
+        <button
+          onClick={handleUpdateStudentInfo}
+          className="px-3 py-1 text-white bg-blue-500 rounded-xl"
+        >
+          {!editing ? "수정" : "완료"}
+        </button>
+      </td>
+    </tr>
+  );
+}
--- pages/teacher/manage/student.jsx
+++ pages/teacher/manage/student.jsx
@@ -1,8 +1,93 @@
+import StudentTable from "../../../components/teacher/manage/student/StudentTable";
 import TSideBar from "../../../components/teacher/TSideBar";
+import { useState } from "react";
 export default function Student() {
+  const [selectedStudent, setSelectedStudents] = useState([]);
+
+  function handleSelectedStudents(_selId) {
+    if (selectedStudent.some((item) => item.id === _selId)) {
+      setSelectedStudents(
+        selectedStudent.filter((item) => {
+          return item.id !== _selId;
+        })
+      );
+    } else {
+      setSelectedStudents([...selectedStudent, { id: _selId }]);
+    }
+  }
+
+  function handleSelectAll(_isSelectedAllState) {
+    if (_isSelectedAllState) {
+      setSelectedStudents(
+        studentsInfo.map((item) => {
+          return { id: item.id };
+        })
+      );
+    } else {
+      setSelectedStudents([]);
+    }
+  }
+
+  //더미데이터
+  const [studentsInfo, setStudentsInfo] = useState([
+    {
+      id: "d66fd2ec-0a5c-4803-a1ef-5b9e0ecfc845",
+      number: 2501,
+      name: "김홍록",
+      userId: "ebs5231",
+      phoneNumber: "01073115490",
+      accountCreated: "2022-02-07",
+    },
+    {
+      id: "08074c49-578f-4721-9c2e-187b2ccda8da",
+      number: 2505,
+      name: "김홍록",
+      userId: "ebs5231",
+      phoneNumber: "01072115490",
+      accountCreated: "2022-02-07",
+    },
+    {
+      id: "a2dd25c1-cd39-4367-b0d6-cece8a5179b9",
+      number: 2504,
+      name: "김홍록",
+      userId: "ebs5231",
+      phoneNumber: "01072115490",
+      accountCreated: "2022-02-07",
+    },
+    {
+      id: "d54839c4-063f-4ff2-8383-e989abf41426",
+      number: 2503,
+      name: "김홍록",
+      userId: "ebs5231",
+      phoneNumber: "01072115490",
+      accountCreated: "2022-02-07",
+    },
+    {
+      id: "15b50718-fcbc-43b1-acae-85a71a836a84",
+      number: 2502,
+      name: "김홍록",
+      userId: "ebs5231",
+      phoneNumber: "01072115490",
+      accountCreated: "2022-02-07",
+    },
+  ]);
+
   return (
     <div className="flex flex-col h-screen lg:flex-row bg-neutral-100">
       <TSideBar />
+      <section className=" overflow-scroll w-full lg:w-[calc(100vw-256px)]">
+        <section>
+          <h1 className="px-10 mt-16 text-5xl font-bold lg:mt-20 lg:px-20">
+            학생 관리
+          </h1>
+        </section>
+        <StudentTable
+          studentsInfo={studentsInfo}
+          selectedStudent={selectedStudent}
+          handleSelectedStudents={handleSelectedStudents}
+          handleSelectAll={handleSelectAll}
+        />
+      </section>
     </div>
   );
 }
