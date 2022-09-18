17282b9eec0c76368ff3b092931d99277f2ce9b3
HongRok Kim <dev@hongrok.net>
2022-09-19 02:12:23 +0900

    목업제작을 위한 student Home페이지 일부 수정

--- pages/student/home.jsx
+++ pages/student/home.jsx
@@ -11,13 +11,13 @@ export default function StudentHome() {
   });
   const [accountData, setAccountData] = useState({
     name: "김홍록",
-    balance: "200000000000",
+    balance: "2000000",
   });
   const [balanceLogs, setBalanceLogs] = useState([
     {
       id: "dsfafasd",
       senderName: "선생님",
-      receiverName: "김홍록",
+      receiverName: "학생",
       transactionType: "plus",
       price: "10000",
       reason: "송금하기",
@@ -25,8 +25,8 @@ export default function StudentHome() {
     },
     {
       id: "dsadfssdfsfsf",
-      senderName: "김홍록",
-      receiverName: "학생1",
+      senderName: "학생",
+      receiverName: "학생2",
       transactionType: "minus",
       price: "20000",
       reason: "송금하기",
@@ -35,15 +35,15 @@ export default function StudentHome() {
   ]);
 
   return (
-    <AuthRoute isTeacherPage={false}>
-      <div className="fixed top-0 bottom-0 left-0 right-0 flex justify-center bg-neutral-200">
-        <div className="max-w-[800px] w-full bg-neutral-100 overflow-auto pb-32">
-          <h1 className="mt-12 ml-6 text-4xl font-bold">홈</h1>
-          <BalanceDisplay bankInfo={bankInfo} accountData={accountData} />
-          <BalanceLogsPanel bankInfo={bankInfo} balanceLogs={balanceLogs} />
-          <BottomNavBar />
-        </div>
+    // <AuthRoute isTeacherPage={false}>
+    <div className="fixed top-0 bottom-0 left-0 right-0 flex justify-center bg-neutral-200">
+      <div className="max-w-[800px] w-full bg-neutral-100 overflow-auto pb-32">
+        <h1 className="mt-12 ml-6 text-4xl font-bold">홈</h1>
+        <BalanceDisplay bankInfo={bankInfo} accountData={accountData} />
+        <BalanceLogsPanel bankInfo={bankInfo} balanceLogs={balanceLogs} />
+        <BottomNavBar />
       </div>
-    </AuthRoute>
+    </div>
+    // </AuthRoute>
   );
 }
