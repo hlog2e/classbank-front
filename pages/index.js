b6c8731ed62a4e38fbdafd382a7fefa95d4f763e
HongRok Kim <dev@hongrok.net>
2022-09-25 17:50:12 +0900

    채널톡 추가

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Head from "next/head";
 import Link from "next/link";
+import { useEffect } from "react";
 import NavBar from "../components/landing/NavBar";
 import Logo from "../components/Logo";
+import { ChannelService } from "../components/common/ChannelTalk";
 
 export default function Home() {
+  useEffect(() => {
+    const CTalk = new ChannelService();
+    CTalk.boot({
+      pluginKey: process.env.NEXT_PUBLIC_CHTALK_PUBLIC_KEY,
+    });
+  }, []);
   return (
-    <div className="bg-white ">
+    <div className="h-screen bg-white ">
       <NavBar />
       <section className="md:h-[500px] h-[600px]   px-8 md:px-20 flex items-center justify-center w-full ">
         <div className=" w-[1100px] h-[450px]  md:flex-row flex-col flex justify-between items-center ">
@@ -20,11 +27,20 @@ export default function Home() {
               <p className="">온라인 송금, 소비, 복권 기능까지!</p>
             </div>
             <div className="flex flex-col justify-between my-4 mt-3 text-center lg:justify-start lg:flex-row h-28 lg:h-fit lg:max-w-none">
-              <Link href="/student">
-                <a className="py-3 mx-1 text-white bg-blue-500 rounded-full px-9">
-                  앱 다운로드
-                </a>
-              </Link>
+              <>
+                <Link href="/download">
+                  <a className="py-3 mx-1 text-white bg-blue-500 rounded-full px-9 lg:hidden">
+                    앱 다운로드
+                  </a>
+                </Link>
+
+                <Link href="/app">
+                  <a className="hidden py-3 mx-1 text-white bg-blue-500 rounded-full px-9 lg:block">
+                    앱 실행
+                  </a>
+                </Link>
+              </>
+
               <Link href="/join">
                 <a className="py-3 mx-1 rounded-full bg-neutral-200 hover:border-blue-500 px-9">
                   회원가입
